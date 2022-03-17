036c69bda450eff092e6a0bc8eed878672f16e6e
ncsicsi <56001584+ncsicsi@users.noreply.github.com>
2022-03-17 11:00:29 +0100

    enemys view

--- SpaceInvaders/View/MainWindow.xaml.cs
+++ SpaceInvaders/View/MainWindow.xaml.cs
@@ -22,18 +22,23 @@ namespace SpaceInvaders.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        int enemyImages;
 
         /// Játékból való kilépés eseménye.
         public event KeyEventHandler KeyIsDown_Event;
         public event KeyEventHandler KeyIsUp_Event;
 
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
             GameCanvas.Focus();
+            makeEnemies(50);
 
         }
+        #endregion
 
+        #region private methods
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
             if (KeyIsDown_Event != null)
@@ -60,5 +65,76 @@ namespace SpaceInvaders.View
                 GameCanvas.Children.Add(newBullet); 
             }
         }
+
+        private void makeEnemies(int enemiCount)
+        {
+            int enemyRow = 0;
+            int left = 600;
+            enemyImages = 0;
+            for (int i =0; i< enemiCount; i++)
+            {
+
+                ImageBrush enemySkin = new ImageBrush();
+                Rectangle newEnemy = new Rectangle
+                {
+                    Tag = "enemy",
+                    Height = 45,
+                    Width = 45,
+                    Fill = enemySkin
+                };
+
+                switch (i)
+                {
+                    case 10:
+                        enemyRow++;
+                        left = 600;
+                        break;
+                    case 20:
+                        enemyRow++;
+                        left = 600;
+                        break;
+                    case 30:
+                        enemyRow++;
+                        left = 600;
+                        break;
+                    case 40:
+                        enemyRow++;
+                        left = 600;
+                        break;
+                }
+
+                Canvas.SetTop(newEnemy, enemyRow*55 + 10);
+                Canvas.SetLeft(newEnemy, left);
+                GameCanvas.Children.Add(newEnemy);
+                
+                left -= 55;
+
+                enemyImages++;
+
+
+                if (enemyImages < 11)
+                {
+                    enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/invadier1.png"));
+                }
+                else if(enemyImages < 21)
+                {
+                    enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/invadier2.png"));
+                }
+                else if(enemyImages < 31)
+                {
+                    enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/invadier2.png"));
+                }
+                else if (enemyImages < 41)
+                {
+                    enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/invadier3.png"));
+                }
+                else if (enemyImages < 51)
+                {
+                    enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/invadier3.png"));
+                }
+
+            }
+        }
+        #endregion
     }
 }
